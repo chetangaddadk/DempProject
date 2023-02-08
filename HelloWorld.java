441b91d45f552a99c153376000f8a88177737b7b
Chetan Gaddad <85189268+chetangaddadk@users.noreply.github.com>
2023-02-08 12:44:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
 class HelloWorld{  
     public static void main(String args[]){  
-<<<<<<< Updated upstream
-     System.out.println("Hello World");
-=======
-     System.out.println("Hello World!!!");
-	 System.out.println("Hello World!!!");
-	 System.out.println("Hello World!!!");
->>>>>>> Stashed changes
+     System.out.println("Hello World  !");
     }  
 }  
